7830d557576165ef552487ccf1424800f0789f9d
Chris Thomas <ccthomas@anl.gov>
2025-04-12 18:03:30 -0500

    feat: init maage design

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./views/**/*.{ejs,html,js}'],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+  plugins: {
+    'postcss-import': {},
+    'postcss-nested': {},
+    tailwindcss: {
+      config: process.env.THEME === 'dark' ? './tailwind.config.dark.mjs' : './tailwind.config.mjs'
     },
-  };
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+    ...(process.env.NODE_ENV === 'production' ? { purgecss } : {})
+  }
+};
