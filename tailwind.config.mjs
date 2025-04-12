8ac5fa9a2ddc1ed3f3859cdd46d542c6595c6fe2
Chris Thomas <ccthomas@anl.gov>
2025-04-12 18:03:30 -0500

    feat: init maage design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import aspectRatio from '@tailwindcss/aspect-ratio';
+import containerQueries from '@tailwindcss/container-queries';
+import lineClamp from '@tailwindcss/line-clamp';
+import animations from '@tailwindcss/animations';
+
 export default {
-  darkMode: ['class', '[data-theme="dark"]'], // if you're using dark mode support
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     extend: {
       fontFamily: {
@@ -10,43 +16,48 @@ export default {
       },
       colors: {
         background: {
-          DEFAULT: "var(--maage-bg-default)",
-          faint: "var(--maage-bg-faint)",
-          subtle: "var(--maage-bg-subtle)",
-          alt: "var(--maage-bg-alt)",
-          muted: "var(--maage-bg-muted)",
-          soft: "var(--maage-bg-soft)",
-          strong: "var(--maage-bg-strong)",
-          inverse: "var(--maage-bg-inverse)",
-          surface: "var(--maage-surface)",
-          border: "var(--maage-border)"
+          DEFAULT: 'var(--maage-bg-default)',
+          faint: 'var(--maage-bg-faint)',
+          subtle: 'var(--maage-bg-subtle)',
+          alt: 'var(--maage-bg-alt)',
+          muted: 'var(--maage-bg-muted)',
+          soft: 'var(--maage-bg-soft)',
+          strong: 'var(--maage-bg-strong)',
+          inverse: 'var(--maage-bg-inverse)',
+          surface: 'var(--maage-surface)',
+          border: 'var(--maage-border)'
         },
         text: {
-          DEFAULT: "var(--maage-text-default)",
-          muted: "var(--maage-text-muted)",
-          subtle: "var(--maage-text-subtle)",
-          inverse: "var(--maage-text-inverse)",
-          link: "var(--maage-text-link)"
+          DEFAULT: 'var(--maage-text-default)',
+          muted: 'var(--maage-text-muted)',
+          subtle: 'var(--maage-text-subtle)',
+          inverse: 'var(--maage-text-inverse)',
+          link: 'var(--maage-text-link)',
+          primary: 'var(--maage-text-primary-color)',
+          secondary: 'var(--maage-text-secondary-color)',
+          tertiary: 'var(--maage-text-tertiary-color)',
+          quaternary: 'var(--maage-text-quaternary-color)',
+          quinary: 'var(--maage-text-quinary-color)'
         },
         success: {
-          50: "var(--maage-success-50)",
-          500: "var(--maage-success-500)",
-          600: "var(--maage-success-600)"
+          50: 'var(--maage-success-50)',
+          500: 'var(--maage-success-500)',
+          600: 'var(--maage-success-600)'
         },
         warning: {
-          50: "var(--maage-warning-50)",
-          500: "var(--maage-warning-500)",
-          600: "var(--maage-warning-600)"
+          50: 'var(--maage-warning-50)',
+          500: 'var(--maage-warning-500)',
+          600: 'var(--maage-warning-600)'
         },
         error: {
-          50: "var(--maage-error-50)",
-          500: "var(--maage-error-500)",
-          600: "var(--maage-error-600)"
+          50: 'var(--maage-error-50)',
+          500: 'var(--maage-error-500)',
+          600: 'var(--maage-error-600)'
         },
         info: {
-          50: "var(--maage-info-50)",
-          500: "var(--maage-info-500)",
-          600: "var(--maage-info-600)"
+          50: 'var(--maage-info-50)',
+          500: 'var(--maage-info-500)',
+          600: 'var(--maage-info-600)'
         },
         primary: {
           50: 'var(--maage-primary-50)',
@@ -59,8 +70,7 @@ export default {
           700: 'var(--maage-primary-700)',
           800: 'var(--maage-primary-800)',
           900: 'var(--maage-primary-900)',
-          950: 'var(--maage-primary-950)',
-          DEFAULT: 'var(--maage-primary-color)'
+          950: 'var(--maage-primary-950)'
         },
         secondary: {
           50: 'var(--maage-secondary-50)',
@@ -73,8 +83,7 @@ export default {
           700: 'var(--maage-secondary-700)',
           800: 'var(--maage-secondary-800)',
           900: 'var(--maage-secondary-900)',
-          950: 'var(--maage-secondary-950)',
-          DEFAULT: 'var(--maage-secondary-color)'
+          950: 'var(--maage-secondary-950)'
         },
         tertiary: {
           50: 'var(--maage-tertiary-50)',
@@ -87,8 +96,46 @@ export default {
           700: 'var(--maage-tertiary-700)',
           800: 'var(--maage-tertiary-800)',
           900: 'var(--maage-tertiary-900)',
-          950: 'var(--maage-tertiary-950)',
-          DEFAULT: 'var(--maage-tertiary-color)'
+          950: 'var(--maage-tertiary-950)'
+        },
+        quaternary: {
+          50: 'var(--maage-quaternary-50)',
+          100: 'var(--maage-quaternary-100)',
+          200: 'var(--maage-quaternary-200)',
+          300: 'var(--maage-quaternary-300)',
+          400: 'var(--maage-quaternary-400)',
+          500: 'var(--maage-quaternary-500)',
+          600: 'var(--maage-quaternary-600)',
+          700: 'var(--maage-quaternary-700)',
+          800: 'var(--maage-quaternary-800)',
+          900: 'var(--maage-quaternary-900)',
+          950: 'var(--maage-quaternary-950)'
+        },
+        quinary: {
+          50: 'var(--maage-quinary-50)',
+          100: 'var(--maage-quinary-100)',
+          200: 'var(--maage-quinary-200)',
+          300: 'var(--maage-quinary-300)',
+          400: 'var(--maage-quinary-400)',
+          500: 'var(--maage-quinary-500)',
+          600: 'var(--maage-quinary-600)',
+          700: 'var(--maage-quinary-700)',
+          800: 'var(--maage-quinary-800)',
+          900: 'var(--maage-quinary-900)',
+          950: 'var(--maage-quinary-950)'
+        },
+        'gray-nickel': {
+          50: 'var(--gray-nickel-50)',
+          100: 'var(--gray-nickel-100)',
+          200: 'var(--gray-nickel-200)',
+          300: 'var(--gray-nickel-300)',
+          400: 'var(--gray-nickel-400)',
+          500: 'var(--gray-nickel-500)',
+          600: 'var(--gray-nickel-600)',
+          700: 'var(--gray-nickel-700)',
+          800: 'var(--gray-nickel-800)',
+          900: 'var(--gray-nickel-900)',
+          950: 'var(--gray-nickel-950)'
         },
         gray: {
           50: 'var(--maage-gray-50)',
@@ -102,9 +149,29 @@ export default {
           800: 'var(--maage-gray-800)',
           900: 'var(--maage-gray-900)',
           950: 'var(--maage-gray-950)'
+        },
+        'sky-gray': {
+          50: 'var(--maage-sky-gray-50)',
+          100: 'var(--maage-sky-gray-100)',
+          200: 'var(--maage-sky-gray-200)',
+          300: 'var(--maage-sky-gray-300)',
+          400: 'var(--maage-sky-gray-400)',
+          500: 'var(--maage-sky-gray-500)',
+          600: 'var(--maage-sky-gray-600)',
+          700: 'var(--maage-sky-gray-700)',
+          800: 'var(--maage-sky-gray-800)',
+          900: 'var(--maage-sky-gray-900)',
+          950: 'var(--maage-sky-gray-950)'
         }
       }
     }
   },
-  plugins: []
-}
+  plugins: [
+    forms,
+    typography,
+    aspectRatio,
+    containerQueries,
+    lineClamp,
+    animations
+  ]
+};
