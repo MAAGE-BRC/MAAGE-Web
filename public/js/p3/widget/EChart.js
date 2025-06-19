77ede7f27fd61fb25af4702c7977b76f9ef52e1d
Chris Thomas <ccthomas@anl.gov>
2025-06-19 11:01:46 -0500

    chore: formatting

--- public/js/p3/widget/EChart.js
+++ public/js/p3/widget/EChart.js
@@ -1,88 +1,76 @@
 define([
-  "dojo/_base/declare",
-  "dijit/_WidgetBase",
-  "dijit/_TemplatedMixin",
-  "dojo/text!./templates/EChart.html",
-  "dojo/_base/lang",
-  "dojo/on",
-  "dojo/dom-construct",
-  "echarts",
-  "maage-themes/maage-echarts-theme",
-  "maage-themes/theme-new",
-], function (
-  declare,
-  _WidgetBase,
-  _TemplatedMixin,
-  template,
-  lang,
-  on,
-  domConstruct,
-  echarts
-) {
-  return declare([_WidgetBase, _TemplatedMixin], {
-    templateString: template,
-    baseClass: "EChart",
-    chart: null,
-    title: "",
+	"dojo/_base/declare",
+	"dijit/_WidgetBase",
+	"dijit/_TemplatedMixin",
+	"dojo/text!./templates/EChart.html",
+	"dojo/_base/lang",
+	"dojo/on",
+	"dojo/dom-construct",
+	"echarts",
+	"maage-themes/maage-echarts-theme",
+	"maage-themes/theme-new",
+], function (declare, _WidgetBase, _TemplatedMixin, template, lang, on, domConstruct, echarts) {
+	return declare([_WidgetBase, _TemplatedMixin], {
+		templateString: template,
+		baseClass: "EChart",
+		chart: null,
+		title: "",
 
-    // The default theme for any chart that doesn't specify one
-    theme: "maage-echarts-theme",
+		theme: "maage-echarts-theme",
 
-    postCreate: function () {
-      this.inherited(arguments);
+		postCreate: function () {
+			this.inherited(arguments);
 
-      if (!this.chartNode) {
-        console.error("EChart: chartNode is not defined in the template.");
-        return;
-      }
+			if (!this.chartNode) {
+				console.error("EChart: chartNode is not defined in the template.");
+				return;
+			}
 
-      // Initialize the ECharts instance. It will use the 'theme' property
-      // passed in during widget construction, or the default defined above.
-      this.chart = echarts.init(this.chartNode, this.theme);
+			this.chart = echarts.init(this.chartNode, this.theme);
 
-      on(
-        window,
-        "resize",
-        lang.hitch(this, function () {
-          if (this.chart) {
-            this.chart.resize();
-          }
-        })
-      );
-    },
+			on(
+				window,
+				"resize",
+				lang.hitch(this, function () {
+					if (this.chart) {
+						this.chart.resize();
+					}
+				})
+			);
+		},
 
-    showLoading: function () {
-      if (this.chart) {
-        this.chart.showLoading();
-      }
-    },
+		showLoading: function () {
+			if (this.chart) {
+				this.chart.showLoading();
+			}
+		},
 
-    hideLoading: function () {
-      if (this.chart) {
-        this.chart.hideLoading();
-      }
-    },
+		hideLoading: function () {
+			if (this.chart) {
+				this.chart.hideLoading();
+			}
+		},
 
-    updateChart: function (data) {
-      console.warn("EChart: updateChart() method not implemented.");
-    },
+		updateChart: function (data) {
+			console.warn("EChart: updateChart() method not implemented.");
+		},
 
-    resize: function () {
-      if (this.chart) {
-        setTimeout(
-          lang.hitch(this, function () {
-            this.chart.resize();
-          }),
-          100
-        );
-      }
-    },
+		resize: function () {
+			if (this.chart) {
+				setTimeout(
+					lang.hitch(this, function () {
+						this.chart.resize();
+					}),
+					100
+				);
+			}
+		},
 
-    destroy: function () {
-      this.inherited(arguments);
-      if (this.chart) {
-        this.chart.dispose();
-      }
-    },
-  });
+		destroy: function () {
+			this.inherited(arguments);
+			if (this.chart) {
+				this.chart.dispose();
+			}
+		},
+	});
 });
--- public/js/p3/widget/EChartStackedBar.js
+++ public/js/p3/widget/EChartStackedBar.js
@@ -1,55 +1,55 @@
 define(["dojo/_base/declare", "./EChart"], function (declare, EChart) {
-  return declare([EChart], {
-    baseClass: "EChartStackedBar",
+	return declare([EChart], {
+		baseClass: "EChartStackedBar",
 
-    updateChart: function (data) {
-      if (!this.chart || !data || !data.series) {
-        this.chart.clear();
-        return;
-      }
+		updateChart: function (data) {
+			if (!this.chart || !data || !data.series) {
+				this.chart.clear();
+				return;
+			}
 
-      const chartSeries = data.series.map((s) => ({
-        name: s.name,
-        type: "bar",
-        stack: "total",
-        emphasis: {
-          focus: "series",
-        },
-        data: s.data,
-      }));
+			const chartSeries = data.series.map((s) => ({
+				name: s.name,
+				type: "bar",
+				stack: "total",
+				emphasis: {
+					focus: "series",
+				},
+				data: s.data,
+			}));
 
-      const option = {
-        title: {
-          text: this.title,
-          left: "center",
-        },
-        tooltip: {
-          trigger: "axis",
-          axisPointer: {
-            type: "shadow",
-          },
-        },
-        legend: {
-          type: "scroll",
-          bottom: 10,
-        },
-        grid: {
-          left: "3%",
-          right: "4%",
-          bottom: "15%",
-          containLabel: true,
-        },
-        xAxis: {
-          type: "category",
-          data: data.categories,
-        },
-        yAxis: {
-          type: "value",
-        },
-        series: chartSeries,
-      };
+			const option = {
+				title: {
+					text: this.title,
+					left: "center",
+				},
+				tooltip: {
+					trigger: "axis",
+					axisPointer: {
+						type: "shadow",
+					},
+				},
+				legend: {
+					type: "scroll",
+					bottom: 10,
+				},
+				grid: {
+					left: "3%",
+					right: "4%",
+					bottom: "15%",
+					containLabel: true,
+				},
+				xAxis: {
+					type: "category",
+					data: data.categories,
+				},
+				yAxis: {
+					type: "value",
+				},
+				series: chartSeries,
+			};
 
-      this.chart.setOption(option);
-    },
-  });
+			this.chart.setOption(option);
+		},
+	});
 });
