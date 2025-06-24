7855e853b7f6fa405eec64346b1b550a09ea49f9
Chris Thomas <ccthomas@anl.gov>
2025-06-23 21:58:58 -0500

    chore: update
    Signed-off-by: Chris Thomas <ccthomas@anl.gov>

--- public/js/p3/widget/GenomeListOverview.js
+++ public/js/p3/widget/GenomeListOverview.js
@@ -404,7 +404,54 @@ define([
 							if (res && res.facet_counts && res.facet_counts.facet_fields[field])
 							{
 								const data = this._processFacets(res.facet_counts.facet_fields[field]);
-								chart.updateChart(data);
+								
+								// Custom configuration for location charts with bottom legend
+								const option = {
+									tooltip: {
+										trigger: "item",
+										formatter: "{b}: {c} ({d}%)",
+									},
+									legend: {
+										type: data.length > 20 ? 'scroll' : 'plain',
+										orient: 'horizontal',
+										bottom: '5%',
+										left: 'center',
+										width: '90%',
+										data: data.map((item) => item.name),
+										itemGap: 8,
+										itemWidth: 18,
+										itemHeight: 10,
+										textStyle: {
+											fontSize: 11
+										},
+										pageButtonItemGap: 5,
+										pageButtonGap: 15,
+										pageIconSize: 12,
+										pageTextStyle: {
+											fontSize: 10
+										}
+									},
+									grid: {
+										top: '10%',
+										bottom: '25%'
+									},
+									series: [
+										{
+											name: "Distribution",
+											type: "pie",
+											radius: ["40%", "60%"],
+											center: ['50%', '40%'],
+											avoidLabelOverlap: false,
+											label: { show: false },
+											emphasis: {
+												label: { show: true, fontSize: "14", fontWeight: "bold" },
+											},
+											labelLine: { show: false },
+											data: data,
+										},
+									],
+								};
+								chart.chart.setOption(option);
 							}
 							chart.hideLoading();
 
