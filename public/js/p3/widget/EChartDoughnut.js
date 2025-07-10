b683b4217e27db3157788c26b865d012a561870c
Chris Thomas <ccthomas@anl.gov>
2025-07-10 12:13:55 -0500

    style: css
    Signed-off-by: Chris Thomas <ccthomas@anl.gov>

--- public/js/p3/widget/EChartDoughnut.js
+++ public/js/p3/widget/EChartDoughnut.js
@@ -20,20 +20,39 @@ define(["dojo/_base/declare", "./EChart"], function (declare, EChart)
 					formatter: "{a} <br/>{b}: {c} ({d}%)",
 				},
 				legend: {
-					orient: "vertical",
-					left: "left",
-					top: "10%",
+					type: data.length > 20 ? 'scroll' : 'plain',
+					orient: 'horizontal',
+					bottom: '5%',
+					left: 'center',
+					width: '90%',
 					data: data.map((item) => item.name),
+					itemGap: 8,
+					itemWidth: 18,
+					itemHeight: 10,
+					textStyle: {
+						fontSize: 11
+					},
+					pageButtonItemGap: 5,
+					pageButtonGap: 15,
+					pageIconSize: 12,
+					pageTextStyle: {
+						fontSize: 10
+					}
+				},
+				grid: {
+					top: '10%',
+					bottom: '25%'
 				},
 				series: [
 					{
 						name: "Status",
 						type: "pie",
-						radius: ["50%", "70%"],
+						radius: ["40%", "60%"],
+						center: ['50%', '40%'],
 						avoidLabelOverlap: false,
 						label: { show: false, position: "center" },
 						emphasis: {
-							label: { show: true, fontSize: "20", fontWeight: "bold" },
+							label: { show: true, fontSize: "14", fontWeight: "bold" },
 						},
 						labelLine: { show: false },
 						data: data,
